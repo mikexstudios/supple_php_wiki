009698951a01d911dcadd3451b384b0932d2c775
Michael Huynh <mike.huynh@gmail.com>
2007-09-05 03:12:51 +0000

    Made suppletext.php config file a bit clearer. Checking in before some large changes.

--- st-system/config/suppletext.php
+++ st-system/config/suppletext.php
@@ -6,6 +6,19 @@
  */  
 $config['wiki_tag'] = ''; 
 
+/**
+ * The following specifies whether or not certain admin pages and options are
+ * disabled. Important in MU environments. Do NOT change this if you don't
+ * know what you are doing.
+ */  
+$config['disable_user_admin'] = false;
+$config['disable_plugin_admin'] = false;
+$config['simple_admin_options'] = false;
+
+/**
+ * Specifies the theme used to display the admin interface. You can drop
+ * the files in /st-system/views/admin/[admin theme name]. Then change it here.
+ */  
 $config['admin_theme'] = 'default';
 
 //We override settings here!
