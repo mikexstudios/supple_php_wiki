aa450f9cf5277cb9fbd3d2e754bf9a1f9ca59477
Michael Huynh <mike.huynh@gmail.com>
2007-09-06 20:56:41 +0000

    Forgot to change the menu_helper over to the new permission system.

--- st-system/helpers/admin/menu_helper.php
+++ st-system/helpers/admin/menu_helper.php
@@ -6,60 +6,44 @@ $CI->load->library('AdminMenu');
 
 //Add menu entries here. We limit the display of some of the menu items by
 //user level.
-$user_role = get_user_role();
 
-//Admin
-if($user_role == 'Administrator')
+if(does_user_have_permission('Registered'))
 {
 	$CI->adminmenu->add_top_level('dashboard', 'Dashboard', 100);
+
+	if($CI->config->item('disable_user_admin') !== true)
+	{
+	$CI->adminmenu->add_top_level('users', 'Users', 400);
+		$CI->adminmenu->add_sub_level('users/profile', 'My Profile', 300, 'users');
+	}
 	
-	$CI->adminmenu->add_top_level('pages', 'Pages', 150);
-		$CI->adminmenu->add_sub_level('pages/permissions', 'Permissions', 100, 'pages');
+	if(does_user_have_permission('Editor'))
+	{
+		$CI->adminmenu->add_top_level('pages', 'Pages', 150);
+			$CI->adminmenu->add_sub_level('pages/permissions', 'Permissions', 100, 'pages');
+	}
 	
-	$CI->adminmenu->add_top_level('presentation', 'Presentation', 200);
-		$CI->adminmenu->add_sub_level('presentation/themes', 'Themes', 100, 'presentation');
+	if(does_user_have_permission('Administrator'))
+	{
+		$CI->adminmenu->add_top_level('presentation', 'Presentation', 200);
+			$CI->adminmenu->add_sub_level('presentation/themes', 'Themes', 100, 'presentation');
+			
+		if($CI->config->item('disable_plugin_admin') !== true)
+		{
+		$CI->adminmenu->add_top_level('plugins', 'Plugins', 300);
+		}
 		
-	if($CI->config->item('disable_plugin_admin') !== true)
-	{
-	$CI->adminmenu->add_top_level('plugins', 'Plugins', 300);
-	}
-	
-	if($CI->config->item('disable_user_admin') !== true)
-	{
-	$CI->adminmenu->add_top_level('users', 'Users', 400);
-		$CI->adminmenu->add_sub_level('users/management', 'Management', 100, 'users');
-		$CI->adminmenu->add_sub_level('users/addnew', 'Add New User', 200, 'users');
-		$CI->adminmenu->add_sub_level('users/profile', 'My Profile', 300, 'users');
-	}
-	
-	$CI->adminmenu->add_top_level('options', 'Options', 500);
-		$CI->adminmenu->add_sub_level('options/general', 'General', 100, 'options');
-		$CI->adminmenu->add_sub_level('options/permissions', 'Permissions', 200, 'options');
-}
-
-if($user_role == 'Editor')
-{
-	$CI->adminmenu->add_top_level('dashboard', 'Dashboard', 100);
-	
-	$CI->adminmenu->add_top_level('pages', 'Pages', 150);
-		$CI->adminmenu->add_sub_level('pages/permissions', 'Permissions', 100, 'pages');
-	
-	if($CI->config->item('disable_user_admin') !== true)
-	{
-	$CI->adminmenu->add_top_level('users', 'Users', 400);
-		$CI->adminmenu->add_sub_level('users/profile', 'My Profile', 300, 'users');
+		if($CI->config->item('disable_user_admin') !== true)
+		{
+			$CI->adminmenu->add_sub_level('users/management', 'Management', 100, 'users');
+			$CI->adminmenu->add_sub_level('users/addnew', 'Add New User', 200, 'users');
+		}
+		
+		$CI->adminmenu->add_top_level('options', 'Options', 500);
+			$CI->adminmenu->add_sub_level('options/general', 'General', 100, 'options');
+			$CI->adminmenu->add_sub_level('options/permissions', 'Permissions', 200, 'options');
 	}
 }
 
-if($user_role == 'Registered')
-{
-	$CI->adminmenu->add_top_level('dashboard', 'Dashboard', 100);
-
-	if($CI->config->item('disable_user_admin') !== true)
-	{
-	$CI->adminmenu->add_top_level('users', 'Users', 400);
-		$CI->adminmenu->add_sub_level('users/profile', 'My Profile', 300, 'users');
-	}
-}
 
 ?>
